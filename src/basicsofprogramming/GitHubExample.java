e7c5b7aa5dd609eb09318bbda6d13b40d5472a97
Ashritha Bhat <yashrithabhat164@gmail.com>
2022-09-16 14:33:37 +0530

    Added another class

---
+++ src/basicsofprogramming/GitHubExample.java
@@ -0,0 +1,10 @@
+package basicsofprogramming;
+
+public class GitHubExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
